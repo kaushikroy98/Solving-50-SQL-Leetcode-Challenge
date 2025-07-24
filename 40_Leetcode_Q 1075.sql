6caf710fae629be16d6df28a97dc3038bf2911d7
Kaushik Roy <84892601+kaushikroy98@users.noreply.github.com>
2025-07-24 14:56:02 +0530

    Create 40_Leetcode_Q 1075.sql

---
+++ 40_Leetcode_Q 1075.sql
@@ -0,0 +1,6 @@
+-- Project Employees I
+
+select project_id, round(sum(experience_years)/count(experience_years),2) as average_years from Project p
+left join Employee e
+on p.employee_id=e.employee_id
+group by project_id;
