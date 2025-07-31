508ebf7f9154a43bd89c42aa929b6ddd66ccf710
Kaushik Roy <84892601+kaushikroy98@users.noreply.github.com>
2025-07-31 19:22:37 +0530

    Create 52_Leetcode_Q 177.sql

---
+++ 52_Leetcode_Q 177.sql
@@ -0,0 +1,13 @@
+-- Nth Highest Salary
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+declare M int;
+set M = N-1;
+ RETURN (
+     # Write your MySQL query statement below.
+     select distinct salary from Employee
+     order by salary desc
+     limit 1 offset M
+ );
+END
